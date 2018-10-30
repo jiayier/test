5f3697aead1e14777bdd85a61eab8e0b52b935d5
root <root@localhost.localdomain>
2018-10-30 10:06:07 +0800

    sl commit

---
+++ Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/18
+ * Time: 11:36
+ * Auther sl
+ */
+namespace app\api\home;
+
+
+class Index
+{
+
+    public function index()
+    {
+        return 'asdfasdf';
+    }
+
+}
